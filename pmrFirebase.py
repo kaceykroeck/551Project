1abafb8e2c28d4fd5e07c64a55482f1756ae21e8
kaceykroeck <98622102+kaceykroeck@users.noreply.github.com>
2022-11-28 12:43:45 -0800

    Add files via upload

---
+++ pmrFirebase.py
@@ -0,0 +1,99 @@
+import requests
+import pandas as pd
+import json
+import sys
+import ast
+import numpy as np
+import mysql.connector
+import warnings
+warnings.filterwarnings(action='ignore')
+
+
+def map_one(y, s, c):
+    partitions = ['p1','p2','p3','p4','p5','p6']
+    prices = []
+    reduce = {}
+    print('Map:')
+    for i in range(len(partitions)):
+        print('Partition:', partitions[i])
+        url1 = "https://final-project-551-ce020-default-rtdb.firebaseio.com"
+        url = url1 + '/user/kacey/beds/city_MR_1_bedroom_n/'+ partitions[i] + '.json'
+        response = requests.get(url)
+        fileDict = ast.literal_eval(response.content.decode('UTF-8'))
+        for i in fileDict:
+            if i['State'] == s and i['CountyName'] == c:
+                want = i[y]
+                print(want)
+                prices.append(want)
+                if reduce == {}:
+                    reduce[y] = [want]
+                else:
+                    reduce[y].append(want)
+    finalReduce = pd.DataFrame(reduce)
+    print()
+    print('Reduce Function:')
+    print(finalReduce)
+
+def map_two(y, s, c):
+    partitions = ['p1','p2','p3','p4','p5','p6','p7','p8','p9','p10']
+    prices = []
+    reduce = {}
+    print('Map:')
+    for i in range(len(partitions)):
+        print('Partition:', partitions[i])
+        url1 = "https://final-project-551-ce020-default-rtdb.firebaseio.com"
+        url = url1 + '/user/kacey/beds/city_MR_2_bedroom_n/'+ partitions[i] + '.json'
+        response = requests.get(url)
+        fileDict = ast.literal_eval(response.content.decode('UTF-8'))
+        for i in fileDict:
+            if i['State'] == s and i['CountyName'] == c:
+                want = i[y]
+                print(want)
+                prices.append(want)
+                if reduce == {}:
+                    reduce[y] = [want]
+                else:
+                    reduce[y].append(want)
+    finalReduce = pd.DataFrame(reduce)
+    print()
+    print('Reduce Function:')
+    print(finalReduce)
+
+def map_three(y, s, c):
+    partitions = ['p1','p2','p3','p4','p5','p6','p7','p8','p9','p10']
+    prices = []
+    reduce = {}
+    print('Map:')
+    for i in range(len(partitions)):
+        print('Partition:', partitions[i])
+        url1 = "https://final-project-551-ce020-default-rtdb.firebaseio.com"
+        url = url1 + '/user/kacey/beds/city_MR_3_bedroom_n/'+ partitions[i] + '.json'
+        response = requests.get(url)
+        fileDict = ast.literal_eval(response.content.decode('UTF-8'))
+        for i in fileDict:
+            if i['State'] == s and i['CountyName'] == c:
+                want = i[y]
+                print(want)
+                prices.append(want)
+                if reduce == {}:
+                    reduce[y] = [want]
+                else:
+                    reduce[y].append(want)
+    finalReduce = pd.DataFrame(reduce)
+    print()
+    print('Reduce Function:')
+    print(finalReduce)
+
+bedroom = str(sys.argv[1])
+state = str(sys.argv[2])
+coname = str(sys.argv[3])
+year = str(sys.argv[4])
+
+if bedroom == '1':
+    map_one(year,state,coname)
+elif bedroom == '2':
+    map_two(year,state,coname)
+elif bedroom == '3':
+    map_three(year,state,coname)
+else:
+    print("Filter search")

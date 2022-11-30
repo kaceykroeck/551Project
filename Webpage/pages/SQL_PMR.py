07b4b927f2a58ab91d23466628537c81042aa386
kaceykroeck <98622102+kaceykroeck@users.noreply.github.com>
2022-11-29 19:07:37 -0800

    Update SQL_PMR.py

--- Webpage/pages/SQL_PMR.py
+++ Webpage/pages/SQL_PMR.py
@@ -77,10 +77,17 @@ def map_three(y, s, c):
     st.write(finalReduce)
     st.write('')
     st.write('Analytics:')
+    st.write('Number of Datapoints:')
+    st.write(len(finalReduce.values)
+    st.write()
+    st.write('Highest Rent:', max(finalReduce.values))
+    st.write('Lowest Rent:', min(finalReduce.values))
     st.write('Average Price the month:', sum(finalReduce.values)/len(finalReduce.values))
     st.write()
-    st.write('Distribution of Price')
+    st.write('Distribution of Price:')
     st.bar_chart(sorted(finalReduce.values))
+    st.write()
+    st.write
 
 db = mysql.connector.connect(**st.secrets["mysql"])
 if db.is_connected():
